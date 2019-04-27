d24b49530c662a1bccc885ab77471286994df993
Klaus Sinani <klaussinani@gmail.com>
2019-04-27 22:37:16 +0300

    Add new `Node#value` class property accessor & mutator (#3)

--- src/node.js
+++ src/node.js
@@ -9,6 +9,14 @@ class Node {
   get key() {
     return this._key;
   }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
 }
 
 module.exports = Node;
