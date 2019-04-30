8717a95cc3921afbc0efd42bc83412a412316f73
Klaus Sinani <klaussinani@gmail.com>
2019-04-30 20:54:37 +0300

    TypeScript: Initialize ambient declarations (#2) (#5)

---
+++ types/mheap.d.ts
@@ -0,0 +1,32 @@
+declare namespace node {
+  export interface Constructor {
+    new <T = any>(key: number, value: T): Instance<T>;
+  }
+
+  export interface Instance<T> {
+    value: T;
+    readonly key: number;
+  }
+}
+
+declare namespace heap {
+  export interface Constructor {
+    new <T = any>(): Instance<T>;
+  }
+
+  export interface Instance<T> {
+    readonly size: number;
+  }
+}
+
+declare namespace mheap {
+  export interface Heap<T = any> extends heap.Instance<T> {}
+  export interface Node<T = any> extends node.Instance<T> {}
+}
+
+declare const mheap: {
+  Heap: heap.Constructor;
+  Node: node.Constructor;
+};
+
+export = mheap;
