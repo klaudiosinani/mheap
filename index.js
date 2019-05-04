6d33b9c01b59dc8144e7b0358f39a994dabe4d7f
Klaus Sinani <klaussinani@gmail.com>
2019-05-04 21:08:52 +0300

    Minor clean-up on `index` module

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-const Heap = require('./src/heap');
 const Node = require('./src/node');
 
-module.exports = Object.assign({}, {Heap}, {Node});
+module.exports = Object.assign({}, {Node});
