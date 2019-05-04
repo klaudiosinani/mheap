1c82ccfefac7c8b3e14a1c6059f3acfec2424594
Klaus Sinani <klaussinani@gmail.com>
2019-05-05 02:43:42 +0300

    Add new `Max#_areOrdered(parentIndex, childIndex)` private class method

--- src/max.js
+++ src/max.js
@@ -2,6 +2,9 @@
 const Heap = require('./heap');
 
 class Max extends Heap {
+  _areOrdered(parentIndex, childIndex) {
+    return this._data[parentIndex].key - this._data[childIndex].key > 0;
+  }
 }
 
 module.exports = Max;
