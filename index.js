8d6705acaf10b08e99dbfcc3b409b5653d3eb6fe
Klaus Sinani <klaussinani@gmail.com>
2019-05-04 21:11:20 +0300

    Export `Min` module class constructor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Max = require('./src/max');
+const Min = require('./src/min');
 const Node = require('./src/node');
 
-module.exports = Object.assign({}, {Max}, {Node});
+module.exports = Object.assign({}, {Max}, {Min}, {Node});
