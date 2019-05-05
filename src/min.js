8dd6023355d869947da570bc5a4d9e3b814631fb
Klaus Sinani <klaussinani@gmail.com>
2019-05-05 20:02:29 +0300

    Add new `Min#_areOrdered(parentIndex, childIndex)` private class method

--- src/min.js
+++ src/min.js
@@ -2,6 +2,9 @@
 const Heap = require('./heap');
 
 class Min extends Heap {
+  _areOrdered(parentIndex, childIndex) {
+    return this._data[parentIndex].key - this._data[childIndex].key < 0;
+  }
 }
 
 module.exports = Min;
