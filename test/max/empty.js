56b4905a1c6cbde4afbcbc1cdd89f91b0dc1211a
Klaus Sinani <klaussinani@gmail.com>
2019-05-09 23:31:14 +0300

    Add test cases for epmty binary max heaps

---
+++ test/max/empty.js
@@ -0,0 +1,91 @@
+'use strict';
+const test = require('ava');
+const {MaxHeap} = require('../../.');
+
+const heap = new MaxHeap();
+
+test('root', t => {
+  t.is(heap.root, undefined);
+});
+
+test('size', t => {
+  t.is(heap.size, 0);
+});
+
+test('clear', t => {
+  t.deepEqual(heap.clear(), heap);
+});
+
+test('extract', t => {
+  t.is(heap.extract(1), undefined);
+  t.deepEqual(heap, heap);
+});
+
+test('extractMax', t => {
+  t.is(heap.extractMax(), undefined);
+  t.deepEqual(heap, heap);
+});
+
+test('fullNodes', t => {
+  t.deepEqual(heap.fullNodes(), []);
+});
+
+test('height', t => {
+  t.is(heap.height(), -1);
+});
+
+test('includes', t => {
+  t.false(heap.includes(10));
+});
+
+test('indexOf', t => {
+  t.is(heap.indexOf(10), -1);
+});
+
+test('internalNodes', t => {
+  t.deepEqual(heap.internalNodes(), []);
+});
+
+test('isEmpty', t => {
+  t.true(heap.isEmpty());
+});
+
+test('keys', t => {
+  t.deepEqual(heap.keys(), []);
+});
+
+test('leafNodes', t => {
+  t.deepEqual(heap.leafNodes(), []);
+});
+
+test('node', t => {
+  t.is(heap.node(0), undefined);
+});
+
+test('partialNodes', t => {
+  t.deepEqual(heap.partialNodes(), []);
+});
+
+test('remove', t => {
+  t.deepEqual(heap.remove(10), heap);
+});
+
+test('search', t => {
+  t.is(heap.search(10), undefined);
+});
+
+test('toArray', t => {
+  t.deepEqual(heap.toArray(), []);
+});
+
+test('toPairs', t => {
+  t.deepEqual(heap.toPairs(), []);
+});
+
+test('update', t => {
+  t.deepEqual(heap.update(0, 'A'), heap);
+});
+
+test('values', t => {
+  t.deepEqual(heap.values(), []);
+});
